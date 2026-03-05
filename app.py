790512c2bdb7b0485fc8d76ec142f59800d4758e
Soham Raorane <79279963+sohamr2@users.noreply.github.com>
2026-03-04 17:18:28 -0700

    update CORS to allow vercel URLs

--- app.py
+++ app.py
@@ -105,6 +105,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
